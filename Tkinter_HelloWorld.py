1ff4bd9347d841ccd0cf7a61c374f6933d63b7fa
Edison <Edisonngunjiri@gmail.com>
2023-07-02 08:58:33 +0300

    This code creates a label with Hello World greating.

--- Tkinter_HelloWorld.py
+++ Tkinter_HelloWorld.py
@@ -1,5 +1,7 @@
-from tkinter import *
-root=Tk()
-label=Label(root,text="Hello Wolrd!")
-label.pack()
-root.mainloop()
+#Python Script to create a label with hello World
+
+from tkinter import *                  #imports the Tkinter library into the global namespace
+root=Tk()                              #Creates root or master application object
+label=Label(root,text="Hello Wolrd!")  #This creates a new Label object.
+label.pack()                           #places the new label widget onto its parent widget
+root.mainloop()                        #This final line starts our main event loop
