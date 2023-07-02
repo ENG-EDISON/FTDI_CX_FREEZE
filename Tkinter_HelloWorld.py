d1c1341529863c49f541ba0d955a6d574dce7ba3
Edison <Edisonngunjiri@gmail.com>
2023-07-02 09:02:07 +0300

    Hello World Label

--- Tkinter_HelloWorld.py
+++ Tkinter_HelloWorld.py
@@ -1,6 +1,7 @@
 #Python Script to create a label with hello World
 
 from tkinter import *                  #imports the Tkinter library into the global namespace
+from tkinter.ttk import *              #This imports the ttk or themed Tk widget library
 root=Tk()                              #Creates root or master application object
 label=Label(root,text="Hello Wolrd!")  #This creates a new Label object.
 label.pack()                           #places the new label widget onto its parent widget
